d96018f3b2f5a99134064bde64d80b6fcba7484d
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-05-10 18:57:12 +0200

    delete a gossip

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -3,7 +3,21 @@ class Gossip < ApplicationRecord
   validates :content, presence: true
 
   belongs_to :user
-  has_many :tags_gossips 
-  has_many :tags, through: :tags_gossips 
+  has_many :tags_gossips, class_name: 'TagGossip', dependent: :destroy
+  has_many :tags, through: :tags_gossips
   has_many :comments
+
+  before_destroy :delete_comments
+  before_destroy :delete_tags
+
+  private
+
+  def delete_comments
+    comments.destroy_all
+  end
+
+  def delete_tags
+    tags.destroy_all
+  end
+  
 end
